478e767e3e09bf1a092efd633e554d84d6014cdd
rishik-m <rishik.makhija2000@gmail.com>
2021-08-25 21:48:20 +0530

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAVSctD22UMX4bAYwNf51argd1B077tMI",
+    authDomain: "connectx-5eb50.firebaseapp.com",
+    projectId: "connectx-5eb50",
+    storageBucket: "connectx-5eb50.appspot.com",
+    messagingSenderId: "658626453826",
+    appId: "1:658626453826:web:f84e6e3854ed477d56e8db",
+    measurementId: "G-LRTYPF4Q88"
+};

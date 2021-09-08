8acbabda6fee0ebc4582e69dc130a77cda0495e1
rishik-m <rishik.makhija2000@gmail.com>
2021-09-08 05:45:32 +0530

    header done!!

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,27 @@ import { Avatar } from '@material-ui/core';
 import React from 'react'
 import styled from 'styled-components';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
         <HeaderSection>
+
            <HeaderLeft>
                <HeaderAvatar />
                <AccessTimeIcon />
            </HeaderLeft>
+
+           <HeaderCenter>
+               <SearchIcon />
+               <input placeholder= "SEARCH" />
+           </HeaderCenter>
+
+           <HeaderRight>
+               <HelpOutlineIcon />
+           </HeaderRight>
+           
         </HeaderSection>
     )
 }
@@ -44,5 +57,37 @@ const HeaderAvatar = styled(Avatar)`
 
    :hover {
       opacity: 0.8;
+   }
+`;
+
+const HeaderCenter = styled.div`
+   flex: 0.4;
+   opacity: 1;
+   border-radius: 6px;
+   border: 1px solid gray;
+   text-align: center;
+   display: flex;
+   padding: 0 50px;
+   color: gray;
+   background-color: #421f44;
+
+   > input {
+       outline: none;
+       background-color: transparent;
+       color: white;
+       border: none;
+       text-align: center;
+       min-width: 30vw;
+   }
+`;
+
+const HeaderRight = styled.div`
+   flex: 0.3;
+   display: flex;
+   align-items: flex-end;
+
+   > .MuiSvgIcon-root {
+       margin-right: 20px;
+       margin-left: auto;
    }
 `;

86fb6c181295e18161d7ce5dc327b26e32044baf
rishik-m <rishik.makhija2000@gmail.com>
2021-09-11 04:05:45 +0530

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBAVSctD22UMX4bAYwNf51argd1B077tMI",
     authDomain: "connectx-5eb50.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
     appId: "1:658626453826:web:f84e6e3854ed477d56e8db",
     measurementId: "G-LRTYPF4Q88"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
